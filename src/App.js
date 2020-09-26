1a5e549b3dd05fb6b640d5b300f76347516fd354
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-09-26 15:27:12 +0700

    Implement font and color  theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { ThemeProvider } from '@xstyled/styled-components'
 import ProductList from './pages/ProductList'
 
+const theme = {
+  colors: {
+    primary: '#F9423A',
+    secondary: '#A23530',
+    mainText: '#424749',
+    subText: '#6E7679',
+    iconBackground: '#e2e4e4',
+    outlineBorder: '#f1f1f2',
+  },
+  fonts: {
+    primary: 'Noto Sans',
+  },
+}
+
 const App = () => {
-  return <ProductList></ProductList>
+  return (
+    <ThemeProvider theme={theme}>
+      <ProductList></ProductList>
+    </ThemeProvider>
+  )
 }
 
 export default App
