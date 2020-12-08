315a5f57eafa111355e675a6210943cde868b898
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-12-08 21:57:32 +0700

    Updates

---
+++ precache-manifest.bb84b3bf52f8c72828554a19f0a31e1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0e17d11011b960acb2c175fb38a26f",
+    "url": "/laprr/index.html"
+  },
+  {
+    "revision": "6851e616461616b2e375",
+    "url": "/laprr/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "efaa7edb805dde421906",
+    "url": "/laprr/static/js/2.4389c968.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/laprr/static/js/2.4389c968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6851e616461616b2e375",
+    "url": "/laprr/static/js/main.c34ef2c3.chunk.js"
+  },
+  {
+    "revision": "1be0828833284ed94cc2",
+    "url": "/laprr/static/js/runtime-main.63044940.js"
+  }
+]);
